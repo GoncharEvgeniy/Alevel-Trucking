eaa24ec699293ffc2ba77732d5c578236481e0be
Jeka <uhp.gonchar.evgeniy@gmail.com>
2019-07-24 20:10:11 +0300

    added class DistanceGetter

--- src/main/java/com/alevel/trucking/model/route/Distance.java
+++ src/main/java/com/alevel/trucking/model/route/Distance.java
@@ -1,7 +1,9 @@
 package com.alevel.trucking.model.route;
 
+import com.alevel.trucking.model.address.Address;
+
 public interface Distance {
 
-    double getDistance();
+    double getDistance(Address addressStart, Address addressEnd);
 
 }
---
+++ src/main/java/com/alevel/trucking/model/route/implementation/DistanceGetter.java
@@ -0,0 +1,28 @@
+package com.alevel.trucking.model.route.implementation;
+
+import com.alevel.trucking.model.address.Address;
+import com.alevel.trucking.model.route.Distance;
+import com.alevel.trucking.model.route.openstreetmap.ru.RequestMatches;
+import com.alevel.trucking.model.route.openstreetmap.ru.ResponseMatches;
+import com.alevel.trucking.model.route.project.osrm.org.RequestRoutes;
+import com.alevel.trucking.model.route.project.osrm.org.ResponseRoutes;
+
+import static java.lang.Thread.sleep;
+
+public class DistanceGetter implements Distance {
+    @Override
+    public double getDistance(Address addressStart, Address addressEnd) {
+        RequestMatches requestMatches = new RequestMatches();
+        ResponseMatches responseMatchesStart = requestMatches.request(addressStart);
+        try {
+            sleep(5000);      // TODO сервер вроде банит по частоте
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        ResponseMatches responseMatchesEnd = requestMatches.request(addressEnd);
+        RequestRoutes requestRoutes = new RequestRoutes();
+        ResponseRoutes responseRoutes = requestRoutes.request(responseMatchesStart.getMatches().get(0),
+                responseMatchesEnd.getMatches().get(0));   // TODO сделать красиво
+        return responseRoutes.getRoutes().get(0).getDistance();  // TODO сделать красиво
+    }
+}

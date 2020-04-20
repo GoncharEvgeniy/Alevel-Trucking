3c091e8a86a8868831c984c066a5f255e98b5f2a
Jeka <uhp.gonchar.evgeniy@gmail.com>
2020-04-20 19:01:33 +0300

    fix access headers

--- src/main/java/com/alevel/trucking/config/WebSecurityConfig.java
+++ src/main/java/com/alevel/trucking/config/WebSecurityConfig.java
@@ -72,8 +72,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
-        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "Authorization", "content-type", "x-auth-token"));
+        configuration.setExposedHeaders(Arrays.asList("authorization", "Authorization"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;

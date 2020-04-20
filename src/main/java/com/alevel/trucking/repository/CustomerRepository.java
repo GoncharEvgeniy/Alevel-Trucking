28a00ec66d04070178fe0bba62fe7d2d1c592b6d
Jeka <uhp.gonchar.evgeniy@gmail.com>
2020-04-20 12:41:41 +0300

    Delete @Repository annotation from repo interfaces

--- src/main/java/com/alevel/trucking/repository/CustomerRepository.java
+++ src/main/java/com/alevel/trucking/repository/CustomerRepository.java
@@ -2,9 +2,7 @@ package com.alevel.trucking.repository;
 
 import com.alevel.trucking.model.person.customer.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
     Customer findByUsername(String username);
--- src/main/java/com/alevel/trucking/repository/UserRepository.java
+++ src/main/java/com/alevel/trucking/repository/UserRepository.java
@@ -2,9 +2,7 @@ package com.alevel.trucking.repository;
 
 import com.alevel.trucking.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByUsername(String username);

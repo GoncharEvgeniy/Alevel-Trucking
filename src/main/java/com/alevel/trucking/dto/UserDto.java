eb6686dcd8fe042ad58e549f646436c5afcd0b89
Jeka <uhp.gonchar.evgeniy@gmail.com>
2020-06-29 19:10:44 +0300

    Add more user info for front end

--- src/main/java/com/alevel/trucking/dto/UserDto.java
+++ src/main/java/com/alevel/trucking/dto/UserDto.java
@@ -10,15 +10,30 @@ public class UserDto {
 
     private String lastName;
 
+    private String secondName;
+
+    private String email;
+
+    private String phone;
+
     private String role;
 
     public UserDto() {
     }
 
-    public UserDto(String login, String firstName, String lastName, String role) {
+    public UserDto(String login,
+                   String firstName,
+                   String lastName,
+                   String secondName,
+                   String email,
+                   String phone,
+                   String role) {
         this.login = login;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.secondName = secondName;
+        this.email = email;
+        this.phone = phone;
         this.role = role;
     }
 
@@ -27,6 +42,9 @@ public class UserDto {
         this.login = user.getUsername();
         this.lastName = user.getLastName();
         this.role = user.getRole().getName();
+        this.secondName = user.getSecondName();
+        this.email = user.getEmail();
+        this.phone = user.getPhone();
     }
 
 
@@ -60,5 +78,29 @@ public class UserDto {
 
     public void setRole(String role) {
         this.role = role;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 }
